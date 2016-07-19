2fedf79f317244038d3664bdeb844576f3562252
Gregory J. Worrall <me@gregjw.com>
2016-07-19 16:42:12 +0100

    Update stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-	:publishable_key => "pk_live_x1IT6fBOSyZylizTTj2wIVZe",
-	:secret_key 	 => "sk_live_2MUQgDAqSU7kSo4aCbXcTmgc"
+	:publishable_key => "",
+	:secret_key 	 => ""
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
